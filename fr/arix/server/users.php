2171e0446a138dbb8d915aa3854174eac024e988
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:47:10 +0200

    Mise à jour de users.php

--- fr/arix/server/users.php
+++ fr/arix/server/users.php
@@ -8,26 +8,36 @@ return [
     'no-users' => 'Il semble que vous n\'ayez aucun sous-utilisateur.',
 
     'name' => 'Nom',
-    'email' => 'Email',
-    '2FA-enabled' => '2FA activé',
+    'email' => 'E-mail',
+    '2FA-enabled' => '2FA activée',
     'creation-date' => 'Date de création',
     'modify-permissions' => 'Modifier les permissions',
 
-    'user-email' => 'Email de l\'utilisateur',
-    'user-email-description' => 'Entrez l\'adresse e-mail de l\'utilisateur que vous souhaitez inviter en tant que sous-utilisateur pour ce serveur.',
+    'user-email' => 'E-mail de l\'utilisateur',
+    'user-email-description' => 'Entrez l\'adresse e-mail de l\'utilisateur que vous souhaitez inviter comme sous-utilisateur pour ce serveur.',
 
-    'select-all-permissions' => 'Sélectionner toutes les autorisations',
+    'select-all-permissions' => 'Sélectionner toutes les permissions',
     'modify-permissions-for' => 'Modifier les permissions pour',
     'view-permissions-for' => 'Voir les permissions pour',
     'create-new-subuser' => 'Créer un nouveau sous-utilisateur',
     'must-not-exceed' => 'Les adresses e-mail ne doivent pas dépasser 191 caractères.',
     'valid-email' => 'Une adresse e-mail valide doit être fournie.',
     'save' => 'Enregistrer',
-    'invite-user' => 'Inviter l\'utilisateur',
+    'invite-user' => 'Inviter un utilisateur',
 
-    'only-permissions-you-assigned' => 'Seules les permissions qui vous sont actuellement attribuées peuvent être sélectionnées lors de la création ou de la modification d\'autres utilisateurs.',
+    'only-permissions-you-assigned' => 'Seules les permissions attribuées à votre compte peuvent être sélectionnées lors de la création ou modification d\'autres utilisateurs.',
 
     'delete-this-subuser' => 'Supprimer ce sous-utilisateur ?',
     'yes-remove' => 'Oui, supprimer le sous-utilisateur',
-    'are-you-sure-to-remove' => 'Êtes-vous sûr de vouloir supprimer ce sous-utilisateur ? Il aura immédiatement révoqué tout accès à ce serveur.'
+    'are-you-sure-to-remove' => 'Êtes-vous sûr de vouloir supprimer ce sous-utilisateur ? Tous ses accès à ce serveur seront immédiatement révoqués.',
+
+    'presets' => [
+        'title' => 'Préréglages',
+        'description' => 'Créez des préréglages pour attribuer rapidement des permissions aux sous-utilisateurs.',
+        'close' => 'Fermer',
+        'no-presets' => 'Aucun préréglage pour le moment.',
+
+        'permissions' => 'Permissions',
+        'create-preset' => 'Créer un préréglage',
+    ]
 ];
