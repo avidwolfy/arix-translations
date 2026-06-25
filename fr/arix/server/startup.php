bbdfb89db59360d25e6a2cd2c762179206755f5e
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:46:50 +0200

    Mise à jour de startup.php

--- fr/arix/server/startup.php
+++ fr/arix/server/startup.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'startup-settings' => 'Paramètres de Démarrage',
-    'startup-command' => 'Commande de Démarrage',
+    'startup-settings' => 'Paramètres de démarrage',
+    'startup-command' => 'Commande de démarrage',
     'docker-image' => 'Image Docker',
-    'read-only' => 'Lecture Seule',
-    'select-docker-feature' => 'Ceci est une fonctionnalité avancée vous permettant de sélectionner une image Docker à utiliser lors de l\'exécution de cette instance de serveur.',
-    'custom-docker-image' => 'L\'image Docker de ce {"server"} a été définie manuellement par un administrateur et ne peut pas être modifiée via cette interface utilisateur.',
-    'variables' => 'Variables',
+    'read-only' => 'Lecture seule',
+    'select-docker-feature' => 'Il s\'agit d\'une fonctionnalité avancée permettant de sélectionner une image Docker à utiliser lors de l\'exécution de cette instance de serveur.',
+    'custom-docker-image' => 'L\'image Docker de ce serveur a été définie manuellement par un administrateur et ne peut pas être modifiée via cette interface.',
+    'variables' => 'Variables'
 ];
