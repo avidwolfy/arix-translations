4a4efc73d373760c7d886b27dede593566e9bad2
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:46:31 +0200

    Mise à jour de settings.php

--- fr/arix/server/settings.php
+++ fr/arix/server/settings.php
@@ -7,7 +7,7 @@ return [
         'title' => 'Détails SFTP',
         'server-address' => 'Adresse du serveur',
         'username' => 'Nom d\'utilisateur',
-        'description' => 'Votre mot de passe SFTP est le même que celui que vous utilisez pour accéder à ce panneau.',
+        'description' => 'Votre mot de passe SFTP est le même que celui que vous utilisez pour accéder à ce panel.',
         'launch-sftp' => 'Lancer SFTP',
     ],
 
@@ -29,9 +29,9 @@ return [
         'reinstall-started' => 'La réinstallation de votre serveur a commencé.',
         'confirm-reinstallation' => 'Confirmer la réinstallation du serveur',
         'yes-reinstall' => 'Oui, réinstaller le serveur',
-        'are-you-sure' => 'Votre serveur sera arrêté et certains fichiers peuvent être supprimés ou modifiés pendant ce processus. Êtes-vous sûr de vouloir continuer ?',
-        'desc-1' => 'La réinstallation de votre serveur l\'arrêtera, puis relancera le script d\'installation qui l\'a initialement configuré.',
-        'desc-2' => 'Certains fichiers peuvent être supprimés ou modifiés pendant ce processus. Veuillez sauvegarder vos données avant de continuer.',
+        'are-you-sure' => 'Votre serveur sera arrêté et certains fichiers pourront être supprimés ou modifiés pendant ce processus. Êtes-vous sûr de vouloir continuer ?',
+        'desc-1' => 'La réinstallation de votre serveur l\'arrêtera, puis relancera le script d\'installation initial.',
+        'desc-2' => 'Certains fichiers peuvent être supprimés ou modifiés pendant ce processus, veuillez sauvegarder vos données avant de continuer.',
         'reinstall-server' => 'Réinstaller le serveur',
     ],
 ];
