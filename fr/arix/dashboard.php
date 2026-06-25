1da55c3a91acf9b944c2ac5f0813a65c61fafd7d
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:38:41 +0200

    Mise à jour de dashboard.php

--- fr/arix/dashboard.php
+++ fr/arix/dashboard.php
@@ -3,17 +3,43 @@
 return [
     'join-our-discord' => 'Rejoignez notre Discord',
     'members-online' => 'Membres en ligne',
-    'billing-area' => 'Espace de facturation',
-    'manage-your-services' => 'Gérez vos services',
-    'supportcenter' => 'Centre de support',
-    'get-support' => 'Obtenir du support',
-    'server-status' => 'Statut du serveur',
-    'check-server-status' => 'Vérifier le statut du serveur',
     'welcome-back' => 'Bon retour',
-    'all-servers-you-have-access-to' => 'Ici, vous pouvez voir tous les serveurs auxquels vous avez accès.',
+    'all-servers-you-have-access-to' => 'Ici vous pouvez voir tous les serveurs auxquels vous avez accès.',
     'your-servers' => 'Affichage de vos serveurs',
     'others-servers' => 'Affichage des serveurs des autres',
     'there-are-no-servers' => 'Il n\'y a aucun autre serveur à afficher.',
     'there-are-no-servers-associated' => 'Aucun serveur n\'est associé à votre compte.',
     'manage-server' => 'Gérer le serveur',
+
+    'select' => 'Sélectionner',
+    'move-folder' => 'Déplacer le dossier',
+
+    'start' => 'Démarrer',
+    'restart' => 'Redémarrer',
+    'stop' => 'Arrêter',
+    'send-command' => 'Envoyer une commande',
+    'create-backup' => 'Créer une sauvegarde',
+    'deselect-all' => 'Tout désélectionner',
+    'deselect' => 'Désélectionner',
+
+    'send' => 'Envoyer',
+
+    'folder' => [
+        'create' => 'Créer un dossier',
+        'edit' => 'Modifier le dossier',
+        'delete' => 'Supprimer le dossier',
+        'are-you-sure-delete' => 'Êtes-vous sûr de vouloir supprimer ce dossier ? Les serveurs à l\'intérieur seront déplacés vers le dossier principal.',
+
+        'name' => 'Nom',
+        'color' => 'Couleur',
+        'icon' => 'Icône',
+        'update' => 'Mettre à jour le dossier',
+
+        'home' => 'Accueil',
+        'home-folder' => 'Dossier principal',
+        'current' => 'Actuel',
+        'no-folders-available' => 'Aucun dossier disponible',
+
+        'move' => 'Déplacer',
+    ]
 ];
