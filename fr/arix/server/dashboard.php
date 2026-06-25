3e2b2ae35a54dc0c456fb2fcae68cdd86c78aec5
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:44:49 +0200

    Mise à jour de dashboard.php

--- fr/arix/server/dashboard.php
+++ fr/arix/server/dashboard.php
@@ -7,11 +7,11 @@ return [
         'title' => 'Détails SFTP',
         'server-address' => 'Adresse du serveur',
         'username' => 'Nom d\'utilisateur',
-        'description' => 'Votre mot de passe SFTP est le même que celui que vous utilisez pour accéder à ce site.',
+        'description' => 'Votre mot de passe SFTP est le même que celui que vous utilisez pour accéder à ce panel.',
         'launch-sftp' => 'Lancer SFTP',
     ],
     'server-info' => [
-        'title' => 'Informations sur le serveur',
+        'title' => 'Informations du serveur',
         'hostname' => 'Nom d\'hôte',
         'node-id' => 'ID du nœud',
         'server-id' => 'ID du serveur',
