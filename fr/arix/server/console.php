9f1510fc1099a1d513d4a0b8c4519d92f2d982af
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:41:23 +0200

    Update console.php

--- fr/arix/server/console.php
+++ fr/arix/server/console.php
@@ -2,12 +2,13 @@
 
 return [
     'console' => 'Console',
-    'node-under-maintenance' => 'Le nœud de ce serveur est actuellement en maintenance, et toutes les actions sont indisponibles.',
-    'running-installation-process' => 'Ce serveur est actuellement en cours d\'exécution de son processus d\'installation, et la plupart des actions sont indisponibles.',
-    'being-transferred' => 'Ce serveur est actuellement en cours de transfert vers un autre nœud, et toutes les actions sont indisponibles.',
-    'view-all' => 'Tout voir',
+    'node-under-maintenance' => 'Le nœud de ce serveur est actuellement en maintenance et toutes les actions sont indisponibles.',
+    'running-installation-process' => 'Ce serveur exécute actuellement son processus d\'installation et la plupart des actions sont indisponibles.',
+    'being-transferred' => 'Ce serveur est actuellement en cours de transfert vers un autre nœud et toutes les actions sont indisponibles.',
+    'type-a-command' => 'Tapez une commande...',
+    'view-all' => 'Voir tout',
     'clear-console' => 'Effacer la console',
-    'share-logs' => 'Partager les journaux',
+    'share-logs' => 'Partager les logs',
     'floating-console' => 'Console flottante',
-    'type-a-command' => 'Saisissez une commande...',
+    'close-console' => 'Fermer la console',
 ];
