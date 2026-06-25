efee81831c3fafa8566a616e6f8ca1333a71a168
avidwolfy <alexis.feutry30@gmail.com>
2026-06-25 17:34:22 +0200

    Mise à jour de activity.php

--- fr/arix/activity.php
+++ fr/arix/activity.php
@@ -6,5 +6,18 @@ return [
     'clear-filters' => 'Effacer les filtres',
     'metadata' => 'Métadonnées',
     'close' => 'Fermer',
-    'no-logs-server' => 'Aucun journal d\'activité disponible pour ce serveur.'
+    'no-logs-server' => 'Aucun journal d\'activité disponible pour ce serveur.',
+
+    'user' => 'Utilisateur',
+    'event' => 'Événement',
+    'ip-address' => 'Adresse IP',
+    'timestamp' => 'Horodatage',
+
+    'filters' => [
+        'title' => 'Filtres d\'activité',
+        'subuser' => 'Sous-utilisateur',
+        'ip-address' => 'Adresse IP',
+        'timestamp_from' => 'Journaux depuis',
+        'timestamp_to' => 'Journaux jusqu’à',
+    ],
 ];
